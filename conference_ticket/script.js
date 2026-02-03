1b4fc70a6b028d401d40731dafa0ca20e9eb5f44
selvija44 <145205633+selvija44@users.noreply.github.com>
2026-02-03 12:31:28 +0530

    done

--- conference_ticket/script.js
+++ conference_ticket/script.js
@@ -4,3 +4,19 @@ const filebox = document.querySelector(".boxfile");
 filebox.addEventListener("click", () => {
     file.click();
 });
+
+
+const namee = document.getElementById('errorname');
+const emaile = document.getElementById('erroremail');
+const errorgh = document.getElementById('errorgh');
+
+function validate(name,email,gh) {
+    if (name === " "){
+        namee.innerHTML = `<span id="errorname">Name can't be empty</span>`
+    }
+    if (email === " ") or (email.contains !== "@"){
+        emaile.innerHTML = `<span id="erroremail">Email can't be empty</span>`
+    }
+    if (errorgh === " ") or (errorgh.contains !== "@github")
+    
+}
